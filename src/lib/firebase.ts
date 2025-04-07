b0bd9b2704fef05a3145b82dff0476727a4fa5db
orelbarclay <coolbarclay@gmail.com>
2025-04-07 13:45:57 -0400

    fix: update Firebase configuration for custom domain auth

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,7 +12,7 @@ import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "mathsandmelodyacademy.com",
+  authDomain: "mathandmelody-a677f.firebaseapp.com",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
